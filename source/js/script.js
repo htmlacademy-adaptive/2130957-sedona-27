9020e57f2c334af9e850a43460ca5999bb7307fb
alex-kabanin <alexcobainius@gmail.com>
2022-12-05 21:26:18 +0400

    refactor of code

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 let mainNavigationToggle = document.querySelector('.main-navigation__toggle');
 let mainNavigation = document.querySelector('.main-navigation');
 
+mainNavigationToggle.style.display = "block";
 mainNavigation.style.position = "absolute";
 mainNavigation.classList.remove('main-navigation--active');
 mainNavigationToggle.onclick = function () {
