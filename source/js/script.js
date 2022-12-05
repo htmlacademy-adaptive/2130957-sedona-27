e2c58392272955673d1087b7f842f63ff2a5f4aa
alex-kabanin <alexcobainius@gmail.com>
2022-12-05 19:05:39 +0400

    layout updated

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 let mainNavigationToggle = document.querySelector('.main-navigation__toggle');
 let mainNavigation = document.querySelector('.main-navigation');
 
+mainNavigation.style.position = "absolute";
 mainNavigation.classList.remove('main-navigation--active');
 mainNavigationToggle.onclick = function () {
   mainNavigation.classList.toggle('main-navigation--active');
