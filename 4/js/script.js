cf9435a99c2eaf0b786f35f43855fe25a9b24f05
Keks <keks@htmlacademy.ru>
2022-12-06 17:52:54 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,6 +1,10 @@
 let mainNavigationToggle = document.querySelector('.main-navigation__toggle');
 let mainNavigation = document.querySelector('.main-navigation');
+let mainHeaderLogo = document.querySelector('.main-header__logo');
+let mainHeader = document.querySelector('.main-header');
 
+mainHeader.style.minHeight = "56px";
+mainHeaderLogo.style.top = "0";
 mainNavigationToggle.style.display = "block";
 mainNavigation.style.position = "absolute";
 mainNavigation.classList.remove('main-navigation--active');
