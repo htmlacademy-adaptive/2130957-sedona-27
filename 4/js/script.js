8c82843de354f530e69ba80fbcb5afeb359b6d81
Keks <keks@htmlacademy.ru>
2022-12-07 20:10:26 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -2,6 +2,7 @@ let mainNavigationToggle = document.querySelector('.main-navigation__toggle');
 let mainNavigation = document.querySelector('.main-navigation');
 let mainHeaderLogo = document.querySelector('.main-header__logo');
 let mainHeader = document.querySelector('.main-header');
+let hearts = document.querySelectorAll('.photo__item-likes');
 
 mainHeader.style.minHeight = "56px";
 mainHeaderLogo.style.top = "0";
@@ -10,3 +11,13 @@ mainNavigation.classList.remove('main-navigation--active');
 mainNavigationToggle.onclick = function () {
   mainNavigation.classList.toggle('main-navigation--active');
 };
+for (let heart of hearts) {
+  heart.onclick = function () {
+    if (heart.classList.contains('photo__item-liked')) {
+      heart.textContent--;
+    } else {
+      heart.textContent++;
+    }
+    heart.classList.toggle('photo__item-liked');
+  };
+}
