4680ebba1594cb14783704a50ac28b5f88ccde24
alex-kabanin <alexcobainius@gmail.com>
2022-12-12 12:15:45 +0400

    form updated

--- source/js/script.js
+++ source/js/script.js
@@ -6,13 +6,13 @@ let likes = document.querySelectorAll('.photo__item-likes');
 let success = document.querySelector('.success');
 let buttonSuccess = document.querySelector('#success-button');
 let submit = document.querySelector('#submit');
+let form = document.querySelector('.form__container');
+let error = document.querySelector('.error');
+let buttonError = document.querySelector('#error-button');
 let firstName = document.querySelector('#first-name');
 let lastName = document.querySelector('#last-name');
 let phoneNumber = document.querySelector('#phone-number');
 let email = document.querySelector('#email');
-let form = document.querySelector('.form__container');
-let error = document.querySelector('.error');
-let buttonError = document.querySelector('#error-button');
 
 mainHeader.style.minHeight = "56px";
 mainHeaderLogo.style.top = "0";
@@ -40,11 +40,14 @@ buttonError.onclick = function () {
   error.classList.remove('error--active');
 }
 
-submit.onclick = function () {
-  if (firstName.value.length === 0 || lastName.value.length === 0 || phoneNumber.value.length === 0 || email.value.length === 0) {
+
+form.onsubmit = function (evt) {
+  evt.preventDefault();
+  if (firstName.value === "" || lastName.value === "" || phoneNumber.value === "" || email.value === "") {
     error.classList.add('error--active');
     return false;
   } else {
-    form.submit();
+    success.classList.add('success--active');
+    return true;
   }
-};
+}
