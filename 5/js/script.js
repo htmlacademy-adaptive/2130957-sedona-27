a64c8789ca35d085110ba92a952d9860948d07e4
Keks <keks@htmlacademy.ru>
2022-12-12 17:10:58 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/script.js
+++ 5/js/script.js
@@ -44,10 +44,10 @@ form.onsubmit = function (evt) {
   evt.preventDefault();
   if (firstName.value === "" || lastName.value === "" || phoneNumber.value === "" || email.value === "") {
     error.classList.add('error--active');
-    firstName.classList.add('form__input-required');
-    lastName.classList.add('form__input-required');
-    phoneNumber.classList.add('form__input-required');
-    email.classList.add('form__input-required');
+    firstName.classList.add('form__input--required');
+    lastName.classList.add('form__input--required');
+    phoneNumber.classList.add('form__input--required');
+    email.classList.add('form__input--required');
     return false;
   } else {
     success.classList.add('success--active');
